ec37d0483b8e26afc12c09fbb9908709c6b82cf3
Bluscream <admin@timo.de.vc>
2016-11-03 14:26:41 +0100

    update script

--- plugins/pyTSon/scripts/serverBrowser.py
+++ plugins/pyTSon/scripts/serverBrowser.py
@@ -1,6 +1,7 @@
 from ts3plugin import ts3plugin, PluginHost
 from pytsonui import setupUi
-from PythonQt.QtGui import QDialog, QListWidgetItem, QWidget, QComboBox
+from PythonQt.QtGui import QDialog, QListWidgetItem, QWidget, QComboBox, QPalette, QTableWidgetItem
+from PythonQt.QtCore import Qt
 import ts3, ts3defines, datetime, os, requests, json
 
 class serverBrowser(ts3plugin):
@@ -82,9 +83,42 @@ class ServersDialog(QDialog):
         #ReportDialog.ReasonList.clear()
         self.serverNameModifier.addItems(self.NAME_MODIFIERS)
         #item.setFlags(item.flags() &~ Qt.ItemIsEditable)
-        servers = requests.get(self.API_BASE+"serverlist/?limit=1")
-        self.status.setText("Response from: "+str(servers.status_code)+": "+servers.reason)
-        ts3.printMessageToCurrentTab(str(servers.content))
+        servers = self.requestServers(1)
+
+        self.status.setText("Status: "+servers["status"].title())
+        palette = QPalette()
+        if servers["status"] == "success":
+            palette.setColor(QPalette.Foreground,Qt.green)
+        else:
+            palette.setColor(QPalette.Foreground,Qt.red)
+        self.status.setPalette(palette)
+        _list = self.serverList
+        for key in servers["result"]["data"]:
+            rowPosition = _list.rowCount
+            _list.insertRow(rowPosition)
+            #_list.setFlags()
+            _list.setItem(rowPosition, 0, QTableWidgetItem(key['name']))
+            _list.setItem(rowPosition, 1, QTableWidgetItem(str(key['users'])+' / '+str(key['slots'])))
+            _list.setItem(rowPosition, 2, QTableWidgetItem(str(key['country'])))
+            _list.setItem(rowPosition, 3, QTableWidgetItem(str(key['password'])))
+            _list.setItem(rowPosition, 4, QTableWidgetItem(str(key['createchannels'])))
+            _list.setItem(rowPosition, 5, QTableWidgetItem(str(key['hidden'])))
+            _list.setItem(rowPosition, 6, QTableWidgetItem(key['address']))
+
+    def requestServers(self, page, filters = []):
+
+        servers = requests.get(self.API_BASE+"serverlist/?page="+str(page)+"&limit=1000")
+        self.status.setText("Response from \""+self.API_DOMAIN+"\": "+str(servers.status_code)+": "+servers.reason)
+        palette = QPalette()
+        if servers.status_code == 200:
+            palette.setColor(QPalette.Foreground,Qt.green)
+        else:
+            palette.setColor(QPalette.Foreground,Qt.red)
+        self.status.setPalette(palette)
+        _servers = servers.content.decode('utf-8')
+        __servers = json.loads(_servers)
+        #ts3.printMessageToCurrentTab(str(_servers))
+        return __servers
 
     #def onReasonListItemChanged(self, item):
         #checked = item.checkState() == Qt.Checked
@@ -100,7 +134,7 @@ class ServersDialog(QDialog):
 
         def on_apply_clicked(self):
             schid = ts3.getCurrentServerConnectionHandlerID()
-            ts3.requestSendPrivateTextMsg(schid, "Reported", 0)
+            ts3.requestSendPrivateTextMsg(schid, "Reported", 1)
 
         def on_reload_clicked(self):
-            self.hide()
+            self.requestServers(1)
